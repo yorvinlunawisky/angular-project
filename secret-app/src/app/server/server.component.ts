ba5e58b7f3dbf6985f93f46e404469a3f7872252
yorvinlunawisky <yorvin0506@gmail.com>
2021-02-19 00:28:20 -0400

    Working with components styles.

--- secret-app/src/app/server/server.component.ts
+++ secret-app/src/app/server/server.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component ({
     selector: 'app-server',
-    templateUrl: './server.component.html';
+    templateUrl: './server.component.html'
 })
 
 export class ServerComponent {
-    
+
 }
