2055725d099bc28f9c82c90952e7b2d1f344f74b
yorvinlunawisky <yorvin0506@gmail.com>
2021-02-21 00:20:48 -0400

    Header update.

--- secret-app/src/app/header/header.component.ts
+++ secret-app/src/app/header/header.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
 })
 
 export class HeaderComponent {
-
+    collapsed = true;
 }
