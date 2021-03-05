dc086790e8dc5e5dbe12e5163a701001f42fdcde
yorvinlunawisky <yorvin0506@gmail.com>
2021-03-04 23:08:45 -0400

    Ingredient counter added.

--- secret-app/src/app/recipes/recipe.service.ts
+++ secret-app/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import  { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 import { Recipe } from './recipe.model';
 
@@ -6,8 +7,16 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
  private recipes: Recipe[] = [
-    new Recipe('Sopa Mix', 'This is simply a test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563'),
-    new Recipe('Hamberger', 'This is simply a test','https://assets.metrolatam.com/do/2015/09/18/CPL_VKyUAAAX9sG-900x600.jpg')
+    new Recipe('Sopa Mix', 'This is simply a test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563',
+    [
+      new Ingredient('Meat', 1),
+      new Ingredient('French Fries', 20)
+    ]),
+    new Recipe('Hamberger', 'This is simply a test','https://assets.metrolatam.com/do/2015/09/18/CPL_VKyUAAAX9sG-900x600.jpg',
+    [
+      new Ingredient('Buns', 2),
+      new Ingredient('Meat', 1)
+    ])
   ];
 
   getRecipes() {
