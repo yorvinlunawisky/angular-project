f3282ff0d8b3a5d0f8424746ba6b00ff5aa41432
yorvinlunawisky <yorvin0506@gmail.com>
2021-03-10 17:15:56 -0400

    Separating components into small pieces

---
+++ angular-practice/src/app/courses/courses.component.ts
@@ -0,0 +1,25 @@
+// import { Course } from './../course';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../course';
+import {COURSES} from './../mock-courses';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+
+ courses = COURSES;
+ selectedCourse: Course;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(course: Course): void {
+    this.selectedCourse = course;
+  }
+
+}
---
+++ angular-practice/src/app/mock-courses.ts
@@ -0,0 +1,11 @@
+import { Course } from './course';
+
+export const COURSES: Course[] = [
+  {id: 1, name: "C++"},
+  {id: 2, name: "Angular"},
+  {id: 3, name: "Python"},
+  {id: 4, name: "Java"},
+  {id: 5, name: "Devops"},
+  {id: 6, name: "Cybersecurity"},
+  {id: 7, name: "ML"}
+]
