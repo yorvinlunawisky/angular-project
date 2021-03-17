ef1900cd6b0d986ac4a3ef809666b43be2840f1a
yorvinlunawisky <yorvin0506@gmail.com>
2021-03-16 20:18:41 -0400

    some bugs fixed and Active Item style added.

--- secret-app/src/app/header/header.component.ts
+++ secret-app/src/app/header/header.component.ts
@@ -6,10 +6,5 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 
 export class HeaderComponent {
-    @Output() featureSelected = new EventEmitter<string>();
-
-    onSelect (feature: string) {
-        this.featureSelected.emit(feature);
-    }
-    collapsed = true;
+       collapsed = true;
 }
