244494b8c1c656f89951ae4d1f6e02f981be9f5e
Yorvin Luna Wisky <yorvin0506@gmail.com>
2021-04-02 14:59:37 -0400

    Resetting th eform.

--- secret-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ secret-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -40,14 +40,16 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editedItem) {
       this.slService.updateIngredient(this.editedItemIdex, newIngredient);
     } else {      
-    this.slService.addIngredient(newIngredient);
+      this.slService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
   }
 
   ngOnDestroy() {
