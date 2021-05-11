9311677f92572f38f085fbcd3dd81a0bf390511d
Yorvin Luna Wisky <yorvin0506@gmail.com>
2021-05-10 23:09:45 -0400

    Some comments removed.

--- secret-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ secret-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -29,11 +29,6 @@ export class RecipeEditComponent implements OnInit {
   }
   
   onSubmit() {
-    //const newRecipe = new Recipe(
-      // this.recipeForm.value['name'], 
-      // this.recipeForm.value['description'],
-      // this.recipeForm.value['imagePath'],
-      // this.recipeForm.value['ingredients']);
     if (this.editMode) {
       this.recipeService.updateRecipe(this.id, this.recipeForm.value);
     } else {
